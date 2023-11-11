c96bc8f2ec8500e5803a20e4bac88c9e62eca9b7
Haron123 <im@keyth.me>
2023-11-11 21:01:25 +0100

    split done

--- sortingworkbench/sorter/MergeSort.java
+++ sortingworkbench/sorter/MergeSort.java
@@ -21,7 +21,14 @@ public class MergeSort extends BaseSort
 		// Result of the Splitting
 		List<List<T>> atomicLists = new LinkedList<List<T>>();
 
-		atomicLists.add(toSort);
+		List<T> testList = new LinkedList<T>();
+
+		testList.add((T) "Hi");
+		testList.add((T) "Bye");
+		testList.add((T) "Hi2");
+		testList.add((T) "Bye2");
+		testList.add((T) "Wassup");
+		atomicLists.add(testList);
 
 		split(atomicLists, 0);
 	}
@@ -29,22 +36,23 @@ public class MergeSort extends BaseSort
 	private <T extends Comparable<T>> void split(List<List<T>> atomicLists, int atList)
 	{
 		// Keep Track of the current list
-		List<T> currentList = new LinkedList<T>();
-
-		// Temporary Storage for each Half of a Split Procedure
-		List<T> firstHalf = new LinkedList<T>();
-		List<T> secondHalf = new LinkedList<T>();
+		List<T> currentList;
 
 		// Split the atomiclists further
 		currentList = atomicLists.get(atList);
 		atList = (atList + 1) % atomicLists.size();
 
-		if(currentList.size() <= 1 && atList == atomicLists.size()) 
+		System.out.println(atomicLists.toString()); 
+
+		if(allSizeOne(atomicLists)) 
 		{
 			System.out.println("done");
+
+			System.out.println(atomicLists.toString()); 
+
 			return;
 		}
-		else if(currentList.size() <= 1)
+		else if(currentList.size() == 1)
 		{
 			System.out.println("Found Atomic");
 		}
@@ -52,25 +60,36 @@ public class MergeSort extends BaseSort
 		{
 			int length = currentList.size();
 			int half_length = length / 2;
+
+			atomicLists.add(new LinkedList<T>());
+			atomicLists.add(new LinkedList<T>());
 		
 			for(int i = 0; i < half_length; i++) 
 			{
-				firstHalf.add(currentList.get(i));
+				atomicLists.get(atomicLists.size() - 2).add(currentList.get(i));
 			}
 
 			for(int i = half_length; i < length; i++) 
 			{
-				secondHalf.add(currentList.get(i));
+				atomicLists.get(atomicLists.size() - 1).add(currentList.get(i));
 			}
-
-			atomicLists.add(firstHalf);
-			atomicLists.add(secondHalf);
 			
-			atomicLists.remove(firstHalf);
-			atomicLists.remove(secondHalf);
-
-			split(atomicLists, atList);
+			atomicLists.remove(currentList);
 		}
+		split(atomicLists, atList);
 	}
 
+	private <T extends Comparable<T>> boolean allSizeOne(List<List<T>> atomicLists)
+	{
+		for(int i = 0; i < atomicLists.size(); i++)
+		{
+			if(atomicLists.get(i).size() > 1)
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
 }
+
