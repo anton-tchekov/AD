642dec78a5f5cfd87a297f19826b01a68f060f56
Haron <im@keyth.me>
2023-11-11 19:50:17 +0100

    merge started

--- sortingworkbench/sorter/MergeSort.java
+++ sortingworkbench/sorter/MergeSort.java
@@ -3,6 +3,7 @@ package sortingworkbench.sorter;
 import sortingworkbench.SortingMetrics;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class MergeSort extends BaseSort
@@ -17,6 +18,59 @@ public class MergeSort extends BaseSort
 	public <T extends Comparable<T>> void sort(List<T> toSort,
 		SortingMetrics metrics)
 	{
+		// Result of the Splitting
+		List<List<T>> atomicLists = new LinkedList<List<T>>();
 
+		atomicLists.add(toSort);
+
+		split(atomicLists, 0);
 	}
+
+	private <T extends Comparable<T>> void split(List<List<T>> atomicLists, int atList)
+	{
+		// Keep Track of the current list
+		List<T> currentList = new LinkedList<T>();
+
+		// Temporary Storage for each Half of a Split Procedure
+		List<T> firstHalf = new LinkedList<T>();
+		List<T> secondHalf = new LinkedList<T>();
+
+		// Split the atomiclists further
+		currentList = atomicLists.get(atList);
+		atList = (atList + 1) % atomicLists.size();
+
+		if(currentList.size() <= 1 && atList == atomicLists.size()) 
+		{
+			System.out.println("done");
+			return;
+		}
+		else if(currentList.size() <= 1)
+		{
+			System.out.println("Found Atomic");
+		}
+		else
+		{
+			int length = currentList.size();
+			int half_length = length / 2;
+		
+			for(int i = 0; i < half_length; i++) 
+			{
+				firstHalf.add(currentList.get(i));
+			}
+
+			for(int i = half_length; i < length; i++) 
+			{
+				secondHalf.add(currentList.get(i));
+			}
+
+			atomicLists.add(firstHalf);
+			atomicLists.add(secondHalf);
+			
+			atomicLists.remove(firstHalf);
+			atomicLists.remove(secondHalf);
+
+			split(atomicLists, atList);
+		}
+	}
+
 }
