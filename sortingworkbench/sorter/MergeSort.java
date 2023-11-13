4ac3423b21db2f36b38d4d1e6d79b57d736be2fb
Haronzs <im@keyth.me>
2023-11-13 18:45:11 +0100

    ich hasse merge

--- sortingworkbench/sorter/MergeSort.java
+++ sortingworkbench/sorter/MergeSort.java
@@ -2,7 +2,6 @@ package sortingworkbench.sorter;
 
 import sortingworkbench.SortingMetrics;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -23,14 +22,52 @@ public class MergeSort extends BaseSort
 
 		List<T> testList = new LinkedList<T>();
 
-		testList.add((T) "Hi");
-		testList.add((T) "Bye");
-		testList.add((T) "Hi2");
-		testList.add((T) "Bye2");
-		testList.add((T) "Wassup");
+		testList.add((T) (Integer)1);
+		testList.add((T) (Integer)66);
+		testList.add((T) (Integer)84);
+		testList.add((T) (Integer)4);
+		testList.add((T) (Integer)53);
+		testList.add((T) (Integer)23);
 		atomicLists.add(testList);
 
 		split(atomicLists, 0);
+		merge(atomicLists);
+	}
+
+	private <T extends Comparable<T>> void merge(List<List<T>> atomicLists)
+	{
+		int leftIndex = 0;
+		int rightIndex = 0;
+
+		int leftCurrent = 0;
+		int rightCurrent = 0;
+
+		while(atomicLists.size() > 1)
+		{
+			leftIndex = 0;
+			rightIndex = 1;
+
+			while(atomicLists.get(leftIndex).size() > 0)
+			{
+				if(atomicLists.get(leftIndex).get(leftCurrent).compareTo(atomicLists.get(rightIndex).get(rightCurrent)) <= 0)
+				{
+					atomicLists.get(rightIndex).add(rightCurrent, atomicLists.get(leftIndex).get(leftCurrent));
+					atomicLists.get(leftIndex).remove(leftCurrent);
+				}
+				else
+				{	 
+					rightCurrent++;
+					if(rightCurrent == atomicLists.get(rightIndex).size())
+					{
+						atomicLists.get(rightIndex).add(atomicLists.get(leftIndex).get(leftCurrent));
+						atomicLists.get(leftIndex).remove(leftCurrent);
+					}
+				}
+			}
+			atomicLists.remove(leftIndex); 
+			rightCurrent = 0;
+			leftCurrent = 0;
+		}
 	}
 
 	private <T extends Comparable<T>> void split(List<List<T>> atomicLists, int atList)
@@ -42,19 +79,13 @@ public class MergeSort extends BaseSort
 		currentList = atomicLists.get(atList);
 		atList = (atList + 1) % atomicLists.size();
 
-		System.out.println(atomicLists.toString()); 
-
 		if(allSizeOne(atomicLists)) 
 		{
-			System.out.println("done");
-
-			System.out.println(atomicLists.toString()); 
-
 			return;
 		}
 		else if(currentList.size() == 1)
 		{
-			System.out.println("Found Atomic");
+			//System.out.println("Found Atomic");
 		}
 		else
 		{
