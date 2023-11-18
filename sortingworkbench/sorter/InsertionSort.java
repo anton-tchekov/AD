b5a8ad84804cc0e9efd08d83c35eb3ab800dd9b3
anton-tchekov <anton@tchekov.net>
2023-11-18 22:09:04 +0000

    Alternative Insertion Sort

--- sortingworkbench/sorter/InsertionSort.java
+++ sortingworkbench/sorter/InsertionSort.java
@@ -20,32 +20,43 @@ public class InsertionSort extends BaseSort
 		assert toSort != null;
 		assert metrics != null;
 
-		int alreadySorted = 1;
-		T currentElement = null;
+//		int alreadySorted = 1;
+//		T currentElement = null;
+//
+//		/* Go Through the whole list once */
+//		for(int listIndex = 0; listIndex < toSort.size(); listIndex++)
+//		{
+//			currentElement = toSort.get(listIndex);
+//
+//			/* Sort the current Element into the already Sorted Part */
+//			for(int sortedIndex = 0; sortedIndex < alreadySorted; sortedIndex++)
+//			{
+//				metrics.incrementCompares();
+//				if(currentElement.compareTo(toSort.get(sortedIndex)) < 0 || sortedIndex == alreadySorted-1)
+//				{
+//					/* Found bigger element in the Sorted Part or End of Sorted Part reached, no bigger element found */
+//					if(sortedIndex != listIndex)
+//					{
+//						toSort.remove(listIndex);
+//						metrics.incrementMoves();
+//						toSort.add(sortedIndex, currentElement);
+//						metrics.incrementMoves();
+//					}
+//
+//					alreadySorted++;
+//					break;
+//				}
+//			}
+//		}
 
-		/* Go Through the whole list once */
-		for(int listIndex = 0; listIndex < toSort.size(); listIndex++)
+		int n = toSort.size();
+		for(int i = 1; i < n; ++i)
 		{
-			currentElement = toSort.get(listIndex);
-
-			/* Sort the current Element into the already Sorted Part */
-			for(int sortedIndex = 0; sortedIndex < alreadySorted; sortedIndex++)
+			int j = i;
+			while(j > 0 && compare(toSort, j - 1, j, metrics) > 0)
 			{
-				metrics.incrementCompares();
-				if(currentElement.compareTo(toSort.get(sortedIndex)) < 0 || sortedIndex == alreadySorted-1)
-				{
-					/* Found bigger element in the Sorted Part or End of Sorted Part reached, no bigger element found */
-					if(sortedIndex != listIndex)
-					{
-						toSort.remove(listIndex);
-						metrics.incrementMoves();
-						toSort.add(sortedIndex, currentElement);
-						metrics.incrementMoves();
-					}
-
-					alreadySorted++;
-					break;
-				}
+				swap(toSort, j, j - 1, metrics);
+				--j;
 			}
 		}
 	}
